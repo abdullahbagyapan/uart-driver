a274849afb7ec7501cbfdb069b2bcf02f58fa9a0
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-04 21:58:25 +0300

    Update::UART module source file
    -Implement prototypes function
    --UART_Init() & UART_PutChar()

--- src/UART.c
+++ src/UART.c
@@ -9,7 +9,7 @@
 
 
 // Module's own header
-#include <src/UART.h>
+#include "UART.h"
 
 // AVR libraries
 #include <avr/io.h>
@@ -18,6 +18,36 @@
 
 /*================================== Functions ==================================*/
 
-uint8_t UART_Init() {
+
+
+void UART_Init() {
+
+    // Operation mode: Asynchronous USART
+    UCSR0C &= ~_BV(UMSEL00) | ~_BV(UMSEL01);
     
+    // Set normal mode
+    UCSR0A &= ~_BV(U2X0);
+
+    // Set baudrate
+    UBRR0H = (uint8_t)(UBRR >> 8);
+    UBRR0L = (uint8_t) UBRR;
+
+    // Set frame format: 8data, 1stop bit
+    UCSR0C |= _BV(UCSZ00) | _BV(UCSZ01) | _BV(USBS0);
+    
+    // Enable transmitter
+    UCSR0B |= _BV(TXEN0) | _BV(TXCIE0);
+
+}
+
+
+
+void UART_PutChar(char cData) {
+
+    // Wait for empty transmit buffer
+    while (!(UCSR0A & (1<<UDRE0)));
+
+    // Put data into buffer
+    UDR0 = cData;
+
 }
