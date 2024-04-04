120a1935e17a5e7edff3b23eea82a0edd51d33f2
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-04 16:15:07 +0300

    Add::UART module source file
    -Declare UART_Init function

---
+++ src/UART.c
@@ -0,0 +1,23 @@
+/*
+ @module: UART
+ @author: Abdullah Bagyapan
+ @date:   04/04/2024
+*/
+
+
+/*================================== Libraries ==================================*/
+
+
+// Module's own header
+#include <src/UART.h>
+
+// AVR libraries
+#include <avr/io.h>
+
+
+
+/*================================== Functions ==================================*/
+
+uint8_t UART_Init() {
+    
+}
