6354e4368d8b97c13e171b79a319cdb18da4f50b
Abdullah Bagyapan <abdullahbagyapan@hotmail.com>
2024-04-04 16:10:54 +0300

    Add::UART module header file
    -Add function UART_Init

---
+++ src/UART.h
@@ -0,0 +1,23 @@
+/*
+ @module: UART
+ @author: Abdullah Bagyapan
+ @date:   04/04/2024
+*/
+
+
+
+
+
+
+/*
+ @brief:  Initialize UART module
+
+ @author  Abdullah Bagyapan
+
+ @date    04/04/2024
+
+ @param   None
+
+ @return  If there is no error return 0, otherwise non-zero
+*/
+uint8_t UART_Init();
